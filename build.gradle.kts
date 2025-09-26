735b77a65e1f3361f49674d65da3cc92bc82529e
RedsTom <tom.frefre.13@gmail.com>
2025-09-26 23:30:01 +0200

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("gg.jte.gradle") version "3.1.16"
+}
+
+group = "fr.redstom"
+version = "0.0.1-SNAPSHOT"
+description = "kholles-manager"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("gg.jte:jte-spring-boot-starter-3:3.1.16")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+jte {
+    generate()
+    binaryStaticContent = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
