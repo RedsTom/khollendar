cac7a9fe61f18269e00dce70f0e55808f58ce3e4
RedsTom <tom.frefre.13@gmail.com>
2025-09-26 23:31:20 +0200

    feat(core): add spring boot application

---
+++ src/main/java/fr/redstom/khollesmanager/KhollesManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.redstom.khollesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhollesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhollesManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/redstom/khollesmanager/KhollesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.redstom.khollesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhollesManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
