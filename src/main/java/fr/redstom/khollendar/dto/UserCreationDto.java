8c7bd405656bbad65210d7be7492c47ad349ebbf
RedsTom <tom.frefre.13@gmail.com>
2025-09-27 21:26:11 +0200

    refactor(dto): convert UserCreationDto to a record for improved immutability

--- src/main/java/fr/redstom/khollendar/dto/UserCreationDto.java
+++ src/main/java/fr/redstom/khollendar/dto/UserCreationDto.java
@@ -1,10 +1,8 @@
 package fr.redstom.khollendar.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class UserCreationDto {
+public record UserCreationDto(
     @NotBlank(message = "Le nom d'utilisateur est obligatoire")
-    private String username;
-}
+    String username
+) {}
