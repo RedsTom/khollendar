6d16bf4c99cc2666c56adadc70004e53a270800a
RedsTom <tom.frefre.13@gmail.com>
2025-10-05 17:22:58 +0200

    refactor: streamline index method and remove unused content parameter

--- src/main/java/fr/redstom/khollendar/controller/MainController.java
+++ src/main/java/fr/redstom/khollendar/controller/MainController.java
@@ -1,5 +1,6 @@
 package fr.redstom.khollendar.controller;
 
+import fr.redstom.khollendar.entity.KholleSession;
 import fr.redstom.khollendar.service.KholleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -7,21 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
     private final KholleService kholleService;
 
+    /**
+     * Page d'accueil. Affiche le fonctionnement de l'application et les prochaines kholles.
+     */
     @GetMapping("/")
-    public String index(CsrfToken csrf, Model model) {
+    public String index(Model model, CsrfToken csrf) {
+        List<KholleSession> upcomingSessions = kholleService.getUpcomingKholleSessions(0, 5).getContent();
+
         model.addAttribute("title", "Accueil");
-        model.addAttribute(
-                "content", "Bienvenue sur Khôlle'n'dar, votre application de gestion de khôlles !");
-
-        model.addAttribute(
-                "upcomingSessions", kholleService.getUpcomingKholleSessions(0, 5).getContent());
-
+        model.addAttribute("upcomingSessions", upcomingSessions);
         model.addAttribute("_csrf", csrf);
 
         return "pages/index";
